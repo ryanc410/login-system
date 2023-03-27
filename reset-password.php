ace0ebf6f28bb6dd1cb891f9cd2042e0273deaee
Ryan Cook <80371197+ryanc410@users.noreply.github.com>
2023-03-27 09:18:34 -0500

    Updated 03/27/23

--- reset-password.php
+++ reset-password.php
@@ -1,106 +1,95 @@
 <?php
-// Initialize the session
-session_start();
 
-// Check if the user is logged in, otherwise redirect to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
 
-// Include config file
-require_once "db_connect.php";
+require_once "includes/db_connect.php";
 
-// Define variables and initialize with empty values
 $new_password = $confirm_password = "";
 $new_password_err = $confirm_password_err = "";
 
-// Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    // Validate new password
-    if(empty(trim($_POST["new_password"]))){
-        $new_password_err = "Please enter the new password.";
-    } elseif(strlen(trim($_POST["new_password"])) < 6){
-        $new_password_err = "Password must have atleast 6 characters.";
+    if(empty(trim($_POST["newPass"]))){
+        $new_password_err = "Please enter your new password.";
+    } elseif(strlen(trim($_POST["newPass"])) < 8){
+        $new_password_err = "Password must have atleast 8 characters.";
     } else{
-        $new_password = trim($_POST["new_password"]);
+        $new_password = trim($_POST["newPass"]);
     }
 
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
+    if(empty(trim($_POST["confirmNewPassword"]))){
         $confirm_password_err = "Please confirm the password.";
     } else{
-        $confirm_password = trim($_POST["confirm_password"]);
+        $confirm_password = trim($_POST["confirmNewPassword"]);
         if(empty($new_password_err) && ($new_password != $confirm_password)){
             $confirm_password_err = "Password did not match.";
         }
     }
 
-    // Check input errors before updating the database
     if(empty($new_password_err) && empty($confirm_password_err)){
-        // Prepare an update statement
         $sql = "UPDATE users SET password = ? WHERE id = ?";
-
         if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
 
-            // Set parameters
             $param_password = password_hash($new_password, PASSWORD_DEFAULT);
             $param_id = $_SESSION["id"];
 
-            // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
-                // Password updated successfully. Destroy the session, and redirect to login page
                 session_destroy();
                 header("location: login.php");
                 exit();
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
-
-            // Close statement
             mysqli_stmt_close($stmt);
         }
     }
-
-    // Close connection
     mysqli_close($link);
 }
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Reset Password</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
-    </style>
-</head>
-<body>
-    <div class="wrapper">
-        <h2>Reset Password</h2>
-        <p>Please fill out this form to reset your password.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>New Password</label>
-                <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
-                <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <a class="btn btn-link ml-2" href="index.php">Cancel</a>
-            </div>
-        </form>
+include 'includes/header.php';
+include 'includes/navbar.php';
+?>
+<div class="container-fluid mt-3">
+    <div class="row d-flex justify-content-center">
+        <div class="col-lg-4 col-sm-12">
+            <h2>Reset Password Form</h2>
+            <p>Please fill out this form to reset your password.</p>
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" id="resetPassForm">
+                <div class="mb-3 mt-2">
+                    <label for="newPass" class="mb-1">New Password</label>
+                    <input type="password" class="form-control" id="newPass" name="newPass" value="<?php echo $new_password; ?>">
+                    <span class="invalid-feedback">
+                        <!-- New Password Error -->
+                        <?php if($new_password_err){
+                        echo $new_password_err;
+                        }
+                        ?>
+                    </span>
+                    <div class="new-password-msg"></div>
+                </div>
+                <div class="mb-3">
+                    <label for="confirmNewPassword">Confirm Password</label>
+                    <input type="password" class="form-control" name="confirmNewPassword" id="confirmNewPassword" >
+                    <span class="invalid-feedback">
+                        <!-- Confirm New Password Error -->
+                        <?php if($confirm_password_err){
+                        echo $confirm_password_err;
+                        }  
+                        ?>
+                    </span>
+                </div>
+                <div class="mb-3">
+                    <input type="submit" class="btn btn-primary" id="reset-pass-submit" value="Submit">
+                    <a class="btn btn-link ml-2" onclick="history.back()">Cancel</a>
+                </div>
+            </form>
+        </div>
     </div>
-</body>
-</html>
+</div>
+<?php
+include 'includes/footer.php';
+?>
