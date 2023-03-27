d7e83da7f8e7af16c1eb05cf5c8ac304729e7afe
Ryan Cook <80371197+ryanc410@users.noreply.github.com>
2023-03-27 08:46:44 -0500

    Updated 03/27/23

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,14 +1,11 @@
 <?php
-/* Database credentials. */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'DATABASE_USER_NAME');
 define('DB_PASSWORD', 'DATABASE_USER_PASSWORD');
 define('DB_NAME', 'DATABASE_NAME');
 
-/* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
