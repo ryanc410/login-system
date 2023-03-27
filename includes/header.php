b7ea3c39faeba5f5c2b16c11633f5010868bf0f8
Ryan Cook <80371197+ryanc410@users.noreply.github.com>
2023-03-27 00:13:54 -0500

    Added includes/header.php 03/27/23

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+<!--Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<!-- Custom CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+<!-- Jquery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<!-- Page Title -->
+    <title><?php echo $page_title; ?></title>
+</head>
+<body>
